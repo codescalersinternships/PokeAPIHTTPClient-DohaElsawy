ca6c79c61a75cf9a45dfb8a165a083874f45eb2a
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-22 15:25:28 +0300

    feat: happy test case in newClient function

--- pokeClient/client_test.go
+++ pokeClient/client_test.go
@@ -1,2 +1,20 @@
 package pokeclient
 
+import (
+	"reflect"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	t.Run("initiate a valid client",func(t *testing.T) {
+
+		expectURL := "https://pokeapi.co/api/v2/gender"
+
+		result := NewClient("/gender")
+
+		if !reflect.DeepEqual(expectURL,result.Url) {
+			t.Errorf("%v",result)
+		}
+	})
+
+}
