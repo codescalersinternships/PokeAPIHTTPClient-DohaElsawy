e5419d0b782a9de800aa6fdccb14d11bc6bda327
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-22 15:31:17 +0300

    feat: test for load from env

--- pokeClient/client_test.go
+++ pokeClient/client_test.go
@@ -17,4 +17,22 @@ func TestClient(t *testing.T) {
 		}
 	})
 
+	t.Run("load endpoint from .env and initiate valid client",func(t *testing.T) {
+
+		expectURL := "https://pokeapi.co/api/v2/characters"
+
+		endpoint , err := LoadConfigFromENV("../.env")
+
+		if err != nil {
+			t.Errorf("the error is %v", err)
+		}
+
+		result := NewClient(endpoint)
+
+
+		if !reflect.DeepEqual(expectURL,result.Url) {
+			t.Errorf("%v",result)
+		}
+	})
+
 }
